b8d6fae8f918977fb6811ab663cbe5cfead028d4
danila_charushin <charushin2000@gmail.com>
2022-03-07 16:06:02 +0700

    fix swagger, fix static url

--- config/urls.py
+++ config/urls.py
@@ -40,19 +40,19 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path(r"^signup/?$", views.SignUpView.as_view(), name="signup"),
-    path(r"^login/?$", views.LoginView.as_view(), name="login"),
-    path(r"^categories/?$", views.CategoryView.as_view(), name="categories"),
-    path(r"^categories_groups/?$", views.CategoryView.as_view(), name="categories"),
+    path(r"swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r"swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r"redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(r"signup/", views.SignUpView.as_view(), name="signup"),
+    path(r"login/", views.LoginView.as_view(), name="login"),
+    path(r"categories/", views.CategoryView.as_view(), name="categories"),
+    path(r"categories_groups/", views.CategoryView.as_view(), name="categories"),
     # path(r"^partners/?$", views.PartnerView.as_view(), name="partners"),
     # path(r"^tasks/?$", views.TaskView.as_view(), name="tasks"),
     # path(r"^points/?$", views.PointView.as_view(), name="points"),
     # path(r"^edit_point/?$", views.EditPointView.as_view(), name="edit_point"),
     # path(r"^moderate_task/?$", views.ModerateTaskView.as_view(), name="moderate_task"),
     # path(r"^profile/?$", views.ProfileView.as_view(), name="profile"),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include("rest_framework.urls", namespace="rest_framework"))
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework"))
 ]
