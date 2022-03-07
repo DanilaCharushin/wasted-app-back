dab8c4fc7133c1c894d1ebe19b1544fcb4df64ac
danila_charushin <charushin2000@gmail.com>
2022-03-07 16:11:56 +0700

    fix gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,10 +1,10 @@
 echo " -- BUILD PROGRESS WILL BE DISPLAYED IN LOGS SECTION -- "
 
 echo " -- [INFO] Run collectstatic..."
-python src/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 echo " -- [INFO] Run migrate..."
-python src/manage.py migrate
+python manage.py migrate
 
 echo " -- [INFO] Starting gunicorn with 2 workers..."
-gunicorn src.core.wsgi:application --log-file -
+gunicorn config.wsgi:application --log-file -
